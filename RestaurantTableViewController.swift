e7907d2dc56b0210f1d9adae15102ce4d9e3104e
HansJso <vigorhan@gmail.com>
2016-07-01 14:32:08 +0800

    Add a Mark

--- RestaurantTableViewController.swift
+++ RestaurantTableViewController.swift
@@ -49,6 +49,7 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
         tableView.tableHeaderView = searchController.searchBar
         searchController.searchResultsUpdater = self
         searchController.dimsBackgroundDuringPresentation = false
+        //notging to change just test 
     }
     
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
